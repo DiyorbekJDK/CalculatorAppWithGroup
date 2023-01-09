2330b5d29ae963b0272b41a968aaa51a7ed076d7
DiyorbekJDK <diyorbekdavronov07072007@gmail.com>
2023-01-09 11:58:44 +0500

    Banana

---
+++ app/src/main/java/com/diyorbek/calculator/Phrase.java
@@ -0,0 +1,14 @@
+package com.diyorbek.calculator;
+
+public class Phrase {
+
+    public static void main(String[] args) {
+
+        String life = "";
+        String coding = "";
+
+
+    }
+
+
+}
